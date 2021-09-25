c3b20104add2d4069a78a27d022149f4aa7df83c
Artem Konoval <kaedefromikeda@gmail.com>
2021-09-25 23:46:30 +0300

    Fix Unit 7

--- RegExp,Date.js
+++ RegExp,Date.js
@@ -1,18 +1,18 @@
 /*Дана строка 'ahb acb aeb aeeb adcb axeb'. Напишите регулярное выражение, 
 которое найдет строки ahb, acb, aeb по шаблону: буква 'a', любой символ, буква 'b'*/
-let str = 'ahb acb aeb aeeb adcb axeb';
-let regexp = /a.b/g;
-let result = str.match(regexp);
+const str = "ahb acb aeb aeeb adcb axeb";
+const regexp = /a.b/g;
+const result = str.match(regexp);
 console.log(result);
 
 //Дана строка '2+3 223 2223'. Напишите регулярку, которая найдет строку 2+3, не захватив остальные
-let str2 = '2+3 223 2223';
-let regexp2 = /\d\D\d\s/;
-let res = str2.match(regexp2);
+const str2 = "2+3 223 2223";
+const regexp2 = /2\+3/;
+const res = str2.match(regexp2);
 console.log(res);
 
 //Получить день, месяц и год текущей даты и по отдельности вывести в консоль
-let time = new Date();
+const time = new Date();
 console.log(time.getFullYear());
 console.log(time.getDate());
-console.log(time.getMonth()); //Выведет восьмой месяц, т.к. отсчет начинается с нуля. можно просто добавить +1
+console.log(time.getMonth() + 1); //Выведет восьмой месяц, т.к. отсчет начинается с нуля. можно просто добавить +1
