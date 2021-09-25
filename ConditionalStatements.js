8874acc4826fe4f221c1cde664fc3217f2c7bf14
Artem Konoval <kaedefromikeda@gmail.com>
2021-09-25 23:37:40 +0300

    Fix Unit-3

--- ConditionalStatements.js
+++ ConditionalStatements.js
@@ -2,32 +2,32 @@
 Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
 Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
 Выполнить явное преобразование(number, string, boolean)*/
- 
-let x = "soft" + true;
+
+const x = "soft" + true;
 console.log(x); // softtrue
 
-let a = "5" + 4;
+const a = "5" + 4;
 console.log(a); // 54;
 
-let b = 5 + true;
+const b = 5 + true;
 console.log(b); // 6 true - 1 , false - 0
 
-let c = "3" * true;
+const c = "3" * true;
 console.log(c); // 3
 
-let d = "2" * 2;
+const d = "2" * 2;
 console.log(d); // 4
 
-let e = 2 * false;
+const e = 2 * false;
 console.log(e); // 0
 
-let f = "6" / false;
+const f = "6" / false;
 console.log(f); // infinity Деление на ноль
 
-let g = "4dr" / 2;
+const g = "4dr" / 2;
 console.log(g); // NaN
 
-let h = 8 / true;
+const h = 8 / true;
 console.log(h); // 8
 
 console.log(Boolean(2)); // true
