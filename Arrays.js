89ee4df17b2ac248100fdad6058ebddef51325c6
Artem Konoval <kaedefromikeda@gmail.com>
2021-09-25 23:44:17 +0300

    Fix Unit 5

--- Arrays.js
+++ Arrays.js
@@ -1,38 +1,38 @@
 //Дан массив состоящий из названий фильмов, выполните перебор массива с выводом в консоль названия каждого фильма
-let arr = ["Terminator", "Terminator 2", "Terminator 3"];
-    for(let movie of arr) {
-    console.log(movie);
-    };
+const arr = ["Terminator", "Terminator 2", "Terminator 3"];
+for (const movie of arr) {
+  console.log(movie);
+}
 
 //Дан массив производителей автомобилей, преобразовать массив в строку и обратно в массив
-let carEng = ["VW", "Dodge", "KIA"];
-let str = carEng.join(", ");
+const carEng = ["VW", "Dodge", "KIA"];
+const str = carEng.join(", ");
 console.log(str);
-let againArr = str.split(", ");
+const againArr = str.split(", ");
 console.log(againArr);
 
 //Дан массив имен ваших знакомых, добавить к каждому элементу массива слова hello
-let names = ["Vasya", "Vika", "Inokentiy"];
-let name = names.map((item, index) => (index = true)? (item = "Hello " + item): NaN );
+const names = ["Vasya", "Vika", "Inokentiy"];
+const name = names.map((item) => `Hello ${item}`);
 console.log(name);
 
 //Преобразовать числовой массив в Boolean
-let arr5 = [1, 2, 8];
-let arr6 = arr5.includes(8);
+const arr5 = [1, 2, 8];
+const arr6 = arr5.includes(8);
 console.log(arr6);
 
 //Отсортировать массив [1,6,7,8,3,4,5,6] по убыванию
-let arr1 = [1,6,7,8,3,4,5,6];
-let arr2 = arr1.sort( (a,b) => b - a);
+const arr1 = [1, 6, 7, 8, 3, 4, 5, 6];
+const arr2 = arr1.sort((a, b) => b - a);
 console.log(arr1);
 
 //Отфильтровать массив [1,6,7,8,3,4,5,6] по значению >3
-let arr3 = [1,6,7,8,3,4,5,6];
-let arr4 = arr3.filter( (value) => value > 3)
+const arr3 = [1, 6, 7, 8, 3, 4, 5, 6];
+const arr4 = arr3.filter((value) => value > 3);
 console.log(arr4);
 
 //Написать функцию, которая принимает два параметра - массив и число и выводит индекс элемента массива равный числу
-let arrayHW = [1, 4, 2, 5, 6];
-let n = 5;
-let arrayF = arrayHW.findIndex( (item) => item == n);
-console.log(arrayF); 
+const arrayHW = [1, 4, 2, 5, 6];
+const n = 5;
+const arrayF = arrayHW.findIndex((item) => item === n);
+console.log(arrayF);
