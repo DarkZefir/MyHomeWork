0fb1a6cb9af4252db8126a024c5c6ac83affe95b
Artem Konoval <kaedefromikeda@gmail.com>
2021-09-25 23:45:28 +0300

    Fix Unit 6

--- Prototype.js
+++ Prototype.js
@@ -1,9 +1,8 @@
 //Создать 2 объекта: animal и cat, объект animal добавить свойство move, объект cat должен наследовать свойство move
-let move = +prompt("animal move","")
-let animal = {
-    move, 
-    }
-let cat={
-    __proto__:animal
-    }
-console.log(cat.move)
+class animal {
+  constructor(move) {
+    this.move = move;
+  }
+}
+const cat = new animal("Jump");
+console.log(cat.move);
