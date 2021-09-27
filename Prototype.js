03e99567c681e88b3cbbed039edc07f88c18ea5b
Artem Konoval <kaedefromikeda@gmail.com>
2021-09-27 22:16:50 +0300

    I understood

--- Prototype.js
+++ Prototype.js
@@ -1,8 +1,17 @@
 //Создать 2 объекта: animal и cat, объект animal добавить свойство move, объект cat должен наследовать свойство move
 class Animal {
-  constructor(move) {
-    this.move = move;
+  constructor(options) {
+    this.move = options.move;
   }
 }
-const cat = new Animal("Jump");
-console.log(cat.move);
+
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.move = options.move;
+  }
+}
+const cat = new Cat({
+  move: "Jump",
+});
+console.log(cat);
